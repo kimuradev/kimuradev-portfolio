f206ca04fe03cada0fe24a878d563fc3f65f3583
Leandro Kimura <kimuradev@gmail.com>
2024-09-24 13:12:46 -0300

    add missing keys

--- app/(pages)/experience/menu-list.tsx
+++ app/(pages)/experience/menu-list.tsx
@@ -11,7 +11,7 @@ export default function MenuList({ data, selectedData, handleClick }: MenuListPr
         <ul className="flex lg:flex-col opacity-0 transition-fadein-right overflow-auto"
         style={{ animationDelay: `300ms` }}>
             {data.map(item => (
-                <li onClick={() => handleClick({ id: item.id })} className={`flex items-center gap-2 cursor-pointer border-b-2 lg:border-l-2 lg:border-b-0 border-zinc-400 py-2 px-4 transition duration-300 ease-in hover:bg-zinc-200 hover:border-zinc-700 ${selectedData.id === item.id ? 'nav-selected' : ''}`}>
+                <li key={item.id} onClick={() => handleClick({ id: item.id })} className={`flex items-center gap-2 cursor-pointer border-b-2 lg:border-l-2 lg:border-b-0 border-zinc-400 py-2 px-4 transition duration-300 ease-in hover:bg-zinc-200 hover:border-zinc-700 ${selectedData.id === item.id ? 'nav-selected' : ''}`}>
                     <span className="text-nowrap">{item.companyName}</span>
                 </li>
             )
